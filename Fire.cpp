89292f6166f774d1a16bc3ce0b48755616b05aeb
Ikmindir <zhizhde@gmail.com>
2022-11-17 16:46:45 +0200

    Create Fire.cpp

---
+++ Fire.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+
+   сларк 6 слотов
+
+   */
+
+int main () {
+	int MyPenis;
+	
+	cin >> MyPenis;
+
+	if (MyPenis == 15)
+	{
+		cout << "Yeah";
+	}
+	else
+	{
+		cout << "Sad";
+	}
+
+	return 0;
+}
